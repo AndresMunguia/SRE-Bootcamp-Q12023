2d2e8702e602ebc74eaf68eefeb4d0e2f1929bc2
AndresMunguia <andresmunguiav@gmail.com>
2023-02-19 07:00:32 -0700

    Added funtion to accept request in JSON for one endpoint.

--- routes/protected.js
+++ routes/protected.js
@@ -1,10 +1,15 @@
 import { protectFunction } from '../services/protected';
 
 export const protect = (req, res, next) => {
+  req.get('Accept').includes('application/json')
   let authorization = req.headers.authorization.split('Bearer ')[1];
   let response = {
     "data": protectFunction(authorization)
   };
-  res.send(response);
+  if (req.accepts('application/json')) {
+    res.json(response);
+  } else {
+    res.send(response);
   next();
+  }
 }
