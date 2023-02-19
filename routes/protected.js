f5d72ec2576e2406d2ff49a9fdffcf15fd754c2b
AndresMunguia <andresmunguiav@gmail.com>
2023-02-19 06:15:41 -0700

    Removed console.log functions used during testing.

--- routes/protected.js
+++ routes/protected.js
@@ -2,12 +2,9 @@ import { protectFunction } from '../services/protected';
 
 export const protect = (req, res, next) => {
   let authorization = req.headers.authorization.split('Bearer ')[1];
-  console.log('Header: ' + authorization)
   let response = {
     "data": protectFunction(authorization)
   };
-  console.log(response)
-  console.log(protectFunction(authorization))
   res.send(response);
   next();
 }
--- services/protected.js
+++ services/protected.js
@@ -5,9 +5,7 @@ const secretkey = 'my2w7wjd7yXF64FIADfJxNs1oupTGAuW'
 
 export const protectFunction = (authorization) => {
   if (authorization) {
-    console.log('In-service auth: ' + authorization);
     let auth = jwt.verify(authorization, secretkey, (err, decoded) => {
-      console.log(err)
       if (decoded) {
         return ("You are under protected data");
       };
