4da572cbe2321b988188bcfc295512d0a1a925f8
AndresMunguia <andresmunguiav@gmail.com>
2023-02-19 07:50:33 -0700

    Separated login endpoint into modules, introduces error with json loop.

--- routes/login.js
+++ routes/login.js
@@ -1,43 +1,12 @@
-const mysql = require('mysql');
-const jwt = require('jsonwebtoken');
-
 import { loginFunction } from '../services/login';
 
-
-// DB connection.
-export const connection = mysql.createConnection({
-  host     : 'sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com',
-  user     : 'secret',
-  password : 'jOdznoyH6swQB9sTGdLUeeSrtejWkcw',
-  database : 'bootcamp_tht',
-  port     : '3306'
-});
-
-
-// Endpoint function.
-export const login = (request, response, next) => {
-  let username = request.body.username;
-  let password = request.body.password;
+export const login = (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
  
-  if (username && password) {
-    let hash = loginFunction(username, password);
-		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, hash], function(error, results, fields) {
-			if (error) throw error;
-			if (results.length > 0) {
-				let data = JSON.stringify(results);
-				let parsed  = JSON.parse(data);
-				let roleID = parsed[0].role;
-				let token = jwt.sign({ roleID }, "my2w7wjd7yXF64FIADfJxNs1oupTGAuW");
-				response.send({
-					"data": token
-				});
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password.');
-		response.end();
-	}
+  let response = {
+    "data": loginFunction(username, password)
+  };
+  res.send(response);
+  next();
 }
--- services/login.js
+++ services/login.js
@@ -1,6 +1,20 @@
 const crypto = require("crypto");
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
 
-export const loginFunction = (username, password) => {
+
+// DB connection.
+export const connection = mysql.createConnection({
+  host     : 'sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com',
+  user     : 'secret',
+  password : 'jOdznoyH6swQB9sTGdLUeeSrtejWkcw',
+  database : 'bootcamp_tht',
+  port     : '3306'
+});
+
+
+// Adding the salt.
+export const addingSalt = (username, password) => {
   
   let salt;
   if (username === 'admin') {
@@ -19,4 +33,27 @@ export const loginFunction = (username, password) => {
 
 return (hashpass)
 
+}
+
+// Endpoint function.
+export const loginFunction = (username, password) => {
+  if (username, password) {
+    let hash = addingSalt (username, password);
+    let token = connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, hash], function(error, results, fields) {
+			if (error) throw error;
+			if (results.length > 0) {
+				let data = JSON.stringify(results);
+				let parsed  = JSON.parse(data);
+				let roleID = parsed[0].role;
+				let signedtoken = jwt.sign({ roleID }, "my2w7wjd7yXF64FIADfJxNs1oupTGAuW");
+        //console.log(authToken)
+        return signedtoken;
+			} else {
+				return ('Please enter valid credential.');
+			}		
+		})
+    return token
+    //console.log(hash)
+    //console.log(token)
+	}
 }
